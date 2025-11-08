a421fd5d6154b285ab3921c11d98744534cc2dac
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2025-11-08 17:14:08 -0300

    improved get request

--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Convidado, Grupo
 
 def home(request, codigo_acesso):
     if request.method == 'POST':
         pass
         
     if request.method == 'GET':
-        return render(request, 'core/home.html', {'codigo_acesso': codigo_acesso})
+        convidados_do_grupo = Convidado.objects.filter(grupo__codigo_acesso=codigo_acesso)
+        grupo = Grupo.objects.filter(codigo_acesso=codigo_acesso).first()
+        request.convidados = convidados_do_grupo
+        request.grupo = grupo
+        return render(request, 'core/home.html', {'codigo_acesso': codigo_acesso, 'request': request})
