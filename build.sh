b6a2aeb18ae6a567e1aa7f0e9fd3fd762d4436af
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2025-12-02 10:43:28 -0300

    Import order fixed

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
-#!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
 tailwindcss -i theme/static_src/src/styles.css -o theme/static/css/dist/styles.css --minify
 
-# Collect static files for production
 python manage.py collectstatic --noinput
 
-# Apply migrations
 python manage.py migrate
