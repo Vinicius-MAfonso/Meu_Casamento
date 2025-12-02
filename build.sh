331942a01e850e6eba4ee2100c6a8a9e1fc783c8
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2025-12-02 10:40:46 -0300

    build update

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+tailwindcss -i theme/static_src/src/styles.css -o theme/static/css/dist/styles.css --minify
+
 # Collect static files for production
 python manage.py collectstatic --noinput
 
