4c3857a93e5f577e1476959d5ed1310a27b25c83
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2026-02-09 23:23:06 -0300

    admin improved

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
 from .models import Grupo, Convidado
 
-admin.site.register(Grupo)
-admin.site.register(Convidado)
+class ConvidadoInline(admin.TabularInline):
+    model = Convidado
+    extra = 1  
+    classes = ('collapse',)
+
+
+@admin.register(Grupo)
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome', 
+        'codigo_acesso', 
+        'status_confirmacao', 
+        'total_convidados'
+    )
+    list_filter = ('status_confirmacao',)
+    search_fields = ('nome', 'codigo_acesso')
+    
+    readonly_fields = ('codigo_acesso',)
+    
+    inlines = [ConvidadoInline]
+
+    def total_convidados(self, obj):
+        return obj.convidados.count()
+    total_convidados.short_description = "Qtd. Convidados"
+
+
+@admin.register(Convidado)
+class ConvidadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome', 
+        'grupo', 
+        'is_representante', 
+        'status_confirmacao'
+    )
+    list_filter = (
+        'status_confirmacao', 
+        'is_representante', 
+        'grupo'
+    )
+    search_fields = ('nome', 'grupo__nome')
+    
+    autocomplete_fields = ['grupo'] 
+    
+    list_editable = ('status_confirmacao',)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 import uuid
 from django.db import models
-from django.utils.text import slugify
 
 
 class Grupo(models.Model):
